e1056536858c4d26d7bd5da9dcdfbdb54145cb99
Cami Rodriguez <camiepisode@outlook.com>
2017-05-08 22:44:34 -0500

    improved for preventing bugs

--- wwwroot/Angular/AdminVoucher/Controller.js
+++ wwwroot/Angular/AdminVoucher/Controller.js
@@ -7,22 +7,26 @@ Flinger.controller("AdminVoucherController", function ($scope, AdminVoucherServi
     }
 
     $scope.GenerateEarlyBird = function () {
-        $Flinger.Loader.Init();
-        AdminVoucherService.GenerateEarlyBird($scope.AdminVoucher.Email).then(function (response) {
-            console.log(response)
-            if (response.data.success != undefined) {
-                if (response.data.success == false) {
-                    $Flinger.Dialog.SetData("Oops, some errors", response.data.message);
-                }
-                else {
-                    $scope.AdminVoucher = {};
-                    $Flinger.Dialog.SetData("Voucher generated!", 'Your Early Bird Access is: ' + response.data.result.StripeData.id);
-                    $Flinger.Loader.Finish();
-                }
+        if ($scope.AdminVoucher.Email != undefined && $scope.AdminVoucher.Email !== null) {
+            if ($scope.AdminVoucher.Email, length > 0) {
+                $Flinger.Loader.Init();
+                AdminVoucherService.GenerateEarlyBird($scope.AdminVoucher.Email).then(function (response) {
+                    //console.log(response)
+                    if (response.data.success != undefined) {
+                        if (response.data.success == false) {
+                            $Flinger.Dialog.SetData("Oops, some errors", response.data.message);
+                        }
+                        else {
+                            $scope.AdminVoucher = {};
+                            $Flinger.Dialog.SetData("Voucher generated!", 'Your Early Bird Access is: ' + response.data.result.StripeData.id);
+                            $Flinger.Loader.Finish();
+                        }
+                    }
+                },
+                    function (response) {
+                        console.log(response);
+                    });
             }
-        },
-            function (response) {
-                console.log(response);
-            });
+        }
     }
 });
