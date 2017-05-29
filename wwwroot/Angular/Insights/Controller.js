8d46fd61c099520256c1fe7e7770428e938cdc93
Cami Rodriguez <camiepisode@outlook.com>
2017-05-29 00:43:04 -0500

    Added modal for voting

--- wwwroot/Angular/Insights/Controller.js
+++ wwwroot/Angular/Insights/Controller.js
@@ -8,4 +8,28 @@ Flinger.controller("InsightsController", function ($scope, InsightsService) {
         $Flinger.Loader.Finish()
     }
 
+    $scope.VoteForFunctionality = function(){
+        console.log('Vote');
+    }
+
+});
+
+Flinger.directive("comingSoonOnClick", function () {
+    return {
+        restrict: "A",
+        link: function (scope, elem, attrs) {
+            elem.bind('click', function () {
+                $Flinger.Dialog.SetData(
+                    "We are developing this functionality!",
+                    'You can vote if you want as soon posible',
+                    [{
+                        text: 'VOTE UP',
+                        className: $Flinger.Dialog.GetAcceptButtonStyle(),
+                        callback: function () {
+                            scope.VoteForFunctionality();
+                        }
+                    }]);
+            });
+        }
+    }
 });
