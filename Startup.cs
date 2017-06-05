e6599e975e3489031de95702bb0ca31ffb741bb5
Cami Rodriguez <camiepisode@outlook.com>
2017-06-05 14:12:55 -0500

    corrected bugs on saving credit card

--- Startup.cs
+++ Startup.cs
@@ -10,7 +10,9 @@ using Microsoft.Extensions.Logging;
 using CrawlerSite.Models;
 using CrawlerSite.Services;
 using CrawlerSite.Attributes;
+using Microsoft.Extensions.Configuration.UserSecrets;
 
+[assembly: UserSecretsId("aspnet-TestApp-ce345b64-19cf-4972-b34f-d16f2e7976ed")]
 namespace CrawlerSite
 {
     public class Startup
@@ -22,12 +24,16 @@ namespace CrawlerSite
                 .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
                 .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
                 .AddEnvironmentVariables();
-            
+
+            if (env.IsDevelopment())
+            {
+                builder.AddUserSecrets<Startup>();
+                //builder.AddUserSecrets();
+            }
+
+            builder.AddEnvironmentVariables();
+
             Configuration = builder.Build();
-
-            AppSettings.ApiUri = Configuration["ApiUri"] == null ? "https://backend.crawlersite.com/api" : Configuration["ApiUri"];
-
-            AppSettings.Uri = Configuration["Uri"] == null ? "https://backend.crawlersite.com" : Configuration["Uri"];
         }
 
         public IConfigurationRoot Configuration { get; }
@@ -37,9 +43,13 @@ namespace CrawlerSite
         {
             // Add framework services.
             services.AddMvc();
-            
+
             // Registered attribute
             services.AddScoped<AuthenticateAttribute>();
+
+            // Getting settings
+            AppSettings.ApiUri = Configuration["ApiUri"] == null ? "https://backend.crawlersite.com/api" : Configuration["ApiUri"];
+            AppSettings.Uri = Configuration["Uri"] == null ? "https://backend.crawlersite.com" : Configuration["Uri"];
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
